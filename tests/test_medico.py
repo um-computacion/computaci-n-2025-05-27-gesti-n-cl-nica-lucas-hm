a90291304fb1d17ca3f695f9f7de0265e1fbd147
lucas-hm <lucashugomoran12@gmail.com>
2025-06-04 08:32:20 -0300

    codigo (falta conectar archivos)

---
+++ tests/test_medico.py
@@ -0,0 +1,12 @@
+from clases import medico
+import unittest
+from unittest import TestCase
+
+class test_2(unittest.TestCase):
+    def test_medico(self, patch_input):
+        b = self.assertEqual(medico.datos_medico.matr_lgj, 64982)
+        c = self.assertEqual(medico.datos_medico.nomb, "Juan Carlos Peralta")
+        d = self.assertEqual(medico.datos_medico.especialidad, "Cariologo")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_paciente.py
@@ -0,0 +1,5 @@
+import datetime
+import unittest
+from clases import paciente
+from unittest import TestCase
+from datetime import date

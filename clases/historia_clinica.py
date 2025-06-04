4b3cb9b64513cc37ce22810f67bd491f399d4095
lucas-hm <lucashugomoran12@gmail.com>
2025-06-04 16:33:03 -0300

    Create historia_clinica.py

---
+++ clases/historia_clinica.py
@@ -0,0 +1,7 @@
+import paciente
+from paciente import paciente
+
+for i in range(paciente.n):
+    historial = paciente.lista_pacientes
+    Edad = paciente.edad
+    print("el historial de la persona es: ", historial," y la edad es: ", Edad)

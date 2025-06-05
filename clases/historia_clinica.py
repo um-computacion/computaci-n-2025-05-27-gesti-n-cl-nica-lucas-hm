27f7cfca1cb64e23b06f32f1be3d76e4a48e5bfc
lucas-hm <lucashugomoran12@gmail.com>
2025-06-05 17:27:09 -0300

    Update historia_clinica.py

--- clases/historia_clinica.py
+++ clases/historia_clinica.py
@@ -1,7 +1,13 @@
 import paciente
 from paciente import paciente
+from menu import menu
 class historia_clinica:
     for i in range(paciente.n):
         historial = paciente.lista_pacientes
         Edad = paciente.edad
         print("el historial de la persona es: ", historial," y la edad es: ", Edad)
+regresar = int(input("Deseas volver al menu? (Y/n) "))
+if regresar = y:
+    menu.menu
+else:
+    break
